d94b49f3c91ce23d4f4780c00c73eb36ea226e90
Danh4160 <63082166+Danh4160@users.noreply.github.com>
2022-10-02 22:29:25 -0400

    renamed file

--- twoway.py
+++ twoway.py
@@ -1,10 +1,13 @@
 import serial
+import time
 
 arduino = serial.Serial(port='COM3', baudrate=9600, timeout=1)
 
 while True:
     message = input("On or Off: ")
     arduino.write(message.encode())
+    time.sleep(0.5)
+    print(f"Arduino sends '{arduino.readline().decode('ascii')}'")
 
 
 

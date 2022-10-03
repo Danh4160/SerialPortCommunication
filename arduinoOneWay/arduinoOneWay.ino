0e42b70e8f96961e19adb69b1b097b4491427a5a
Danh4160 <63082166+Danh4160@users.noreply.github.com>
2022-10-02 21:42:33 -0400

    one way communication

---
+++ arduinoOneWay/arduinoOneWay.ino
@@ -0,0 +1,19 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.setTimeout(10);
+  pinMode(LED_BUILTIN, OUTPUT); // On board LED
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0) {
+    String str = Serial.readStringUntil('\n');
+    if (str == "on") {
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
+    if (str == "off") {
+      digitalWrite(LED_BUILTIN, LOW);
+    }
+  }
+}

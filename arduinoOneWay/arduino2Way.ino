f15acb9ba04378a308f538242e917c18911e2752
Danh4160 <63082166+Danh4160@users.noreply.github.com>
2022-10-02 22:29:36 -0400

    Create arduino2Way.ino

---
+++ arduinoOneWay/arduino2Way.ino
@@ -0,0 +1,22 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.setTimeout(10);
+  pinMode(LED_BUILTIN, OUTPUT); // On board LED
+  bool twoway = True;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0) {
+    String str = Serial.readStringUntil('\n');
+    if (str == "on") {
+      digitalWrite(LED_BUILTIN, HIGH);
+      if (twoway) Serial.write("Led is on")
+    }
+    if (str == "off") {
+      digitalWrite(LED_BUILTIN, LOW);
+      if (twoway) Serial.write("Led is off")
+    }
+  }
+}
